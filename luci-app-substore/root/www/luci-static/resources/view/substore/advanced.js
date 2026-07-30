5e90413d0c0dc44e26592987f1727663893260c5
第十六夜月 <mashanghai88@gmail.com>
2026-07-31 07:01:04 +0800

    Remove custom backend name and icon options
    Removed custom name and icon options from advanced settings.

--- luci-app-substore/root/www/luci-static/resources/view/substore/advanced.js
+++ luci-app-substore/root/www/luci-static/resources/view/substore/advanced.js
@@ -17,12 +17,6 @@ return view.extend({
 		s = m.section(form.NamedSection, 'config', 'substore', _('高级设置'));
 		s.anonymous = true;
 
-		o = s.option(form.Value, 'backend_custom_name', _('自定义实例名称'), _('显示在前端界面上的后端名称'));
-		o.placeholder = 'OpenWrt';
-
-		o = s.option(form.Value, 'backend_custom_icon', _('自定义图标URL'), _('显示在前端界面上的后端图标'));
-		o.placeholder = 'https://example.com/icon.png';
-
 		o = s.option(form.Value, 'x_powered_by', _('X-Powered-By 响应头'), _('自定义 HTTP 响应头中的 X-Powered-By 字段'));
 		o.placeholder = 'Express';
 

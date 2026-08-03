1c14c884ad281b33cc51a288ad52f90bc11bc3d1
第十六夜月 <mashanghai88@gmail.com>
2026-08-04 02:48:19 +0800

    Update network.js

--- luci-app-substore/root/www/luci-static/resources/view/substore/network.js
+++ luci-app-substore/root/www/luci-static/resources/view/substore/network.js
@@ -68,7 +68,7 @@ return view.extend({
 			return validateDownloadProxy(value);
 		};
 
-		o = s.option(form.Value, 'github_token', _('GitHub 令牌'), _('查询版本号时携带此令牌请求 GitHub API，留空则匿名请求。'));
+		o = s.option(form.Value, 'github_token', _('GitHub 令牌'), _('查询版本号时携带此令牌请求 GitHub API，留空则匿名请求'));
 		o.password = true;
 		o.placeholder = '';
 
